2ee4bb7ce1cb12eed10c1d326bfe676aba287781
Muhammad Khadafi <muhammadkhadafi@Muhammads-MBP.fios-router.home>
2017-09-19 01:00:35 -0400

    come on

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -24,7 +24,7 @@ class DashboardController < ApplicationController
   	# @james = @@james
   	# @jessica = @@jessica
   	# @current_temp = @@current_temp
-  	render plain: "haha"
+  	render
   end
 
   def jennifer
