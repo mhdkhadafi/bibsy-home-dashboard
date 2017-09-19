367619d9eb8df5bea9824018ea8107b3221587b2
Muhammad Khadafi <muhammadkhadafi@Muhammads-MBP.fios-router.home>
2017-09-18 22:45:22 -0400

    delay partial

--- app/assets/javascripts/bibsy-bond.js
+++ app/assets/javascripts/bibsy-bond.js
@@ -1,27 +1,4 @@
-// $(function() {
-//     $('#jennifer_button').click(function() {
-//     	alert("alert")
-//     	$('#jennifer_button').load('/jennifer');
-// });
 
-// $( "#jennifer_button" ).click(function() {
-//   // alert( "Handler for .click() called." );
-//   $('#jennifer_button').load('/jennifer');
-// });
-// $(document).ready(
-// 	function() {
-// 	  setInterval(function() {
-// 	    $('#jennifer_button').load('/jennifer');
-// 	}, 3000);
-// });
-
-// $('#jennifer_button').load('/dashboard_controller/jennifer'); // May need to use whatever locals you need to in here.
-
-// $("#jennifer_button").html("<%= escape_javascript(render partial: 'shared/jennifer') %>");
-
-// function jennifer() {
-// 	$('#jennifer_button').load('/jennifer');
-// }
 
 $(document).ready(function() {
   $.simpleWeather({
@@ -40,3 +17,4 @@ $(document).ready(function() {
     }
   });
 });
+
